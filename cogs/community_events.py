d11d68a724deef5e23ca24b20e96142bc54d79e3
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-04-02 02:01:41 +0200

    Update

--- cogs/community_events.py
+++ cogs/community_events.py
@@ -10,13 +10,11 @@ class on_member(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, user):
         print("Er is een member erbij gekomen!")
-        await self.welcome_member(self, user)
-
-
-    async def welcome_member(self, user):
         embed=disnake.Embed(title=f"A member has joined us!", description=f"Welcome {user.mention}", color=disnake.Color.orange())
         channel_to_send = self.bot.get_channel(ChannelIDs.GENERAL)
         await channel_to_send.send(embed=embed)
+
+
 
 
 
